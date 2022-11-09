25b75c5ac76e9afb3470b831a674f308360b05a7
Арсен Григорян <arsen8577711@yandex.ru>
2022-11-09 12:25:51 +0300

    Сдаю работу на проверку

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -6,4 +6,3 @@ class OwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user)
-
